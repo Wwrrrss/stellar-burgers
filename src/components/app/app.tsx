4c8e5ea852d6da4049f5f1aa239a053931a9dc6f
Tanya <tylucsijc@yandex.ru>
2026-02-20 17:32:00 +0300

    fixed

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -11,10 +11,16 @@ import {
 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+  useParams
+} from 'react-router-dom';
 
 import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from '../../services/store';
 import { fetchIngredients } from '../../services/constructorSlice';
 import { ProtectedRoute } from '../protected-route';
@@ -26,12 +32,31 @@ const App = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const background = location.state?.background;
+  const currentUrl = location.pathname;
 
   useEffect(() => {
     dispatch(fetchIngredients());
     dispatch(getUser());
     dispatch(fetchFeed());
   }, []);
+
+  const getNumber = (url: string, name: string) => {
+    const feedIndex = url.indexOf(name);
+    if (feedIndex !== -1) {
+      let number = url.substring(feedIndex + name.length);
+      while (number.length < 6) number = '0' + number;
+      return number;
+    }
+    return '';
+  };
+
+  const [numberFeed, setNumberFeed] = useState('#');
+  const [numberOrders, setNumberOrders] = useState('#');
+
+  useEffect(() => {
+    setNumberFeed('#' + getNumber(currentUrl, '/feed/'));
+    setNumberOrders('#' + getNumber(currentUrl, '/profile/orders/'));
+  }, [currentUrl]);
 
   return (
     <div className={styles.app}>
@@ -75,7 +100,12 @@ const App = () => {
                     navigate('/feed');
                   }}
                 >
-                  <OrderInfo />
+                  <div>
+                    <p className='text text_type_digits-default'>
+                      {numberFeed}
+                    </p>
+                    <OrderInfo />
+                  </div>
                 </Modal>
               }
             />
@@ -109,7 +139,12 @@ const App = () => {
                     navigate('/profile/orders');
                   }}
                 >
-                  <OrderInfo />
+                  <div>
+                    <p className='text text_type_digits-default'>
+                      {numberOrders}
+                    </p>
+                    <OrderInfo />
+                  </div>
                 </Modal>
               }
             />
@@ -144,7 +179,12 @@ const App = () => {
                       navigate(-1);
                     }}
                   >
-                    <OrderInfo />
+                    <div>
+                      <p className='text text_type_digits-default'>
+                        {numberOrders}
+                      </p>
+                      <OrderInfo />
+                    </div>
                   </Modal>
                 }
               />
@@ -161,7 +201,12 @@ const App = () => {
                       navigate(-1);
                     }}
                   >
-                    <OrderInfo />
+                    <div>
+                      <p className='text text_type_digits-default'>
+                        {numberFeed}
+                      </p>
+                      <OrderInfo />
+                    </div>
                   </Modal>
                 }
               />
